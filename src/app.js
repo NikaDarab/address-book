ceb6781123b8893b2129492f41cf3c27c0a89c41
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2021-01-05 11:06:11 -0600

    app.get

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
+const { v4: uuid } = require("uuid");
 const { NODE_ENV } = require("./config");
 
 const app = express();
@@ -12,14 +13,45 @@ const app = express();
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
+app.use(express.json());
 app.use(helmet());
 app.use(cors());
 
+const addresses = [
+  {
+    id: "ce20079c-2326-4f17-8ac4-f617bfd28b7f",
+    firstName: "nika",
+    lastName: "darab",
+    address1: "2511 w braker ln",
+    address2: "apt 367",
+    city: "austin",
+    state: "tx",
+    zip: "78758",
+  },
+];
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Welcome to Address Book!");
 });
 
-+app.use(function errorHandler(error, req, res, next) {
+app.get("/address", (req, res) => {
+  res.json(addresses);
+});
+
+app.post("/address", (req, res) => {
+  //get
+});
+app.delete("/address/:id", (req, res) => {
+  const { id } = req.params;
+  const index = addresses.findIndex((address) => address.id === id);
+  if (index === -1) {
+    return res.status(400).send("Address not found");
+  }
+  addresses.splice(index, 1);
+  res.status(204).end();
+});
+
+//error handling
+app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
